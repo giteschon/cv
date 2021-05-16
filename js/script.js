91c9c7bf59327b4fd0dfe1f9198150a253cf3b89
Ivana Kasalo <ivana@mileum-technologies.com>
2021-05-16 19:16:40 +0200

    update:navigation sticky

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const SHOW_MORE = "more-text";
 (function () {
     initTheme();
     initSkillsLevel();
+    addActiveNavEvents();
 })();
 
 
@@ -51,6 +52,22 @@ function initSkillsLevel() {
 
 }
 
+function addActiveNavEvents() {
+    let list = document.getElementById("menu").getElementsByTagName("li");   
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].onclick = function (event) {
+            let el = event.target.parentNode;
+            let siblings = el.parentNode.getElementsByTagName("li");
+            console.log(siblings);
+            for (let i = 0; i < siblings.length; i++) {
+                siblings[i].classList.remove("active");
+            }
+            el.classList.add("active");
+        }
+    };
+}
+
 function addSkill(id, level) {
     let el = document.getElementById(id);
 
@@ -65,13 +82,17 @@ function addSkill(id, level) {
 }
 
 function showMore(e) {
-    let list = e.srcElement.previousElementSibling.classList;
+    let btn = e.srcElement;
+    let list = btn.previousElementSibling.classList;
 
     if (list.contains(SHOW_MORE)) {
         list.remove(SHOW_MORE);
+        console.log(btn);
+        btn.innerHTML = "Show more";
     }
     else {
         list.add(SHOW_MORE);
+        btn.innerHTML = "Show less";
     }
 }
 
